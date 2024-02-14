a19bf3aeee33c02327be0df957fa13685364814b
verde <65857286+chorogrim@users.noreply.github.com>
2024-02-14 13:50:03 +0900

    Add files via upload

--- algorithm/queue.py
+++ algorithm/queue.py
@@ -9,47 +9,60 @@
 #         만약 큐에 들어있는 정수가 없을 경우에는 -1을 출력
 # back : 큐의 가장 뒤에 있는 정수를 출력
 #        만약 큐에 들어있는 정수가 없을 경우에는 -1을 출력
-
+# deque는 list와 dictionary와 거의 동일
 
 import sys
 
 # 입력 받을 값 n을 선언
 n = int(sys.stdin.readline())
 
-# 정수르 저장할 큐를 빈 리스트로 선언
+# 정수를 저장할 빈 리스트 선언
 queue = []
 
+# 반복문을 통해 입력 받는 값을 처리
 for i in range(n):
     word = sys.stdin.readline().split()
     order = word[0]
 
+    # order와 push와 같다면
     if order == 'push':
-        value = word[0]
-        queue.append(value)
-
+        # queue에 value의 두 번째 값을 append
+        queue.append(int(word[1]))
+    # order와 pop와 같다면
     elif order == 'pop':
-        if not queue:
-            print(-1)
+        # queue의 길이가 0보다 작다면 queue의 첫 번째 요소를 제거(제일 앞의 요소 삭제)
+        if len(queue) != 0:
+            print(queue.pop(0))
+        # 그렇지 않다면 -1을 출력
         else:
-            print(queue[-1])
+            print(-1)
 
+    # order와 size가 같다면 queue의 정수의 개수를 출력
     elif order == 'size':
         print(len(queue))
-
+    
+    # order와 empty와 같다면
     elif order == 'empty':
-        if not queue:
+        # queue의 길이가 0일 경우 1을 출력, 아니면 0을 출력
+        if len(queue) == 0:
             print(1)
         else:
             print(0)
-
-    elif order == 'front':
-        if not queue:
-            print(-1)
-        else:
-            print(queue[-1])
     
+    # order와 front와 같다면
+    elif order == 'front':
+        # queue의 길이가 0보다 작다면 queue의 첫 번째 값을 출력, 그렇지 않다면 -1을 출력
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+
+    # order와 back이 같다면
     elif order == 'back':
-        if not queue:
+        # queue의 길이가 0보다 작다면 queue의 마지막 값을 출력, 그렇지 않다면 -1을 출력
+        if len(queue) == 0:
             print(-1)
         else:
             print(queue[-1])
+
+## 맞았음 ! ##
