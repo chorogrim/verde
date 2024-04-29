884edcc6203cb26f2c3730a06131ef4237a8b927
verde <65857286+chorogrim@users.noreply.github.com>
2024-04-29 14:10:50 +0900

    Update 이분탐색_입국심사.py

--- programmers/이분탐색_입국심사.py
+++ programmers/이분탐색_입국심사.py
@@ -1,3 +1,8 @@
+# 입국심사를 기다리는 사람 수n, 각 심사관이 한 명을 심사하는데 걸리는 시간이 
+# 담긴 배열 times가 매개변수로 주어질 때, 모든 사람이 심사를 받는데 걸리는
+# 시간의 최솟값을 return 하도록 solution 함수를 작성해 주세요.
+
+
 def solution(n, times):
     start = 1
     end = max(times) * n
