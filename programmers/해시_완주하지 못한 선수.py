1947c020dbf72bf6168a34ac267194408f9477bc
verde <65857286+chorogrim@users.noreply.github.com>
2024-04-29 14:11:08 +0900

    Update 해시_완주하지 못한 선수.py

--- programmers/해시_완주하지 못한 선수.py
+++ programmers/해시_완주하지 못한 선수.py
@@ -1,3 +1,8 @@
+# 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와
+# 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+# 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+
 def solution(participant, completion):
     answer = ''
     p_dict = {}
