8151c966e254d7236d9b642592d94dd7c00fd72c
verde <65857286+chorogrim@users.noreply.github.com>
2024-05-02 15:22:48 +0900

    Update 하노이의 탑.py

--- programmers/하노이의 탑.py
+++ programmers/하노이의 탑.py
@@ -2,19 +2,19 @@
 # n개의 원판을 3번 원판으로 최소로 옮기는 방법을 return하는 solution를 완성해주세요.
 
 def solution(n):
-    answer = []
+    answer = [] # 원판을 옮긴 경로를 저장할 빈 리스트 
     
     def move(n, start, des, temp):
         a = []
-        if(n == 1):
-            a.append(start)
-            a.append(des)
-            answer.append(a)
-            return
-        move(n-1, start, temp, des)
-        a.append(start)
-        a.append(des)
-        answer.append(a)
-        move(n-1, temp, des, start)
-    move(n, 1, 3, 2)
-    return answer
+        if(n == 1): # 만약 옮겨야 할 원판이 1개라면
+            a.append(start) # 출발 기둥 번호를 리스트 a에 추가
+            a.append(des) # 목적지 기둥 번호를 리스트 a에 추가
+            answer.append(a) # 리스트 a를 answer에 추가
+            return # 함수를 종료
+        move(n-1, start, temp, des) # 가장 큰 원판을 제외하고 나머지 원판들을 start에서 temp로 옮김
+        a.append(start) # 가장 큰 원판을 start에서 des로 옮긴 후 출발 기둥 번호를 리스트 a에 추가
+        a.append(des) # 가장 큰 원판을 start에서 des로 옮긴 후 목적지 기둥 번호를 리스트 a에 추가
+        answer.append(a) # 원판을 옮긴 경로를 리스트 a로 표현하여 answer 리스트에 추가
+        move(n-1, temp, des, start) # temp에 옮겨둔 원판들은 목적지 기둥 des로 옮김
+    move(n, 1, 3, 2) # 처음에는 모든 원판이 1번 기둥에 있고, 목적지는 3번 기둥이며, 보조 기둥은 2번
+    return answer # 원판을 옮긴 경로를 담고 있는 answer 리스트를 반환
