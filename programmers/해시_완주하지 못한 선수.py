946a043b3a02a84a60801148512118d205077ff6
verde <65857286+chorogrim@users.noreply.github.com>
2024-05-02 15:16:13 +0900

    Update 해시_완주하지 못한 선수.py

--- programmers/해시_완주하지 못한 선수.py
+++ programmers/해시_완주하지 못한 선수.py
@@ -2,21 +2,20 @@
 # 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
 # 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 
-
 def solution(participant, completion):
-    answer = ''
-    p_dict = {}
+    answer = '' # 반환할 값을 초기화
+    p_dict = {} # 참가자 이름을 키로 하고, 등장 횟수를 값으로 가지는 p_dict 딕셔너리
     
-    for p in participant:
-        if p not in p_dict:
-            p_dict[p] = 1
-        else:
-            p_dict[p] += 1
+    for p in participant: # 참가자 리스트를 반복
+        if p not in p_dict: # 현재 참가자가 딕셔너리에 없다면
+            p_dict[p] = 1 # 딕셔너리에 새로운 키를 추가하고 값을 1로 설정
+        else: # 이미 딕셔너리에 존재하는 참가라자면
+            p_dict[p] += 1 # 해당 참가자의 값을 1증가
             
-    for c in completion:
-        p_dict[c] -= 1
+    for c in completion: # 완주자 리스트를 반복
+        p_dict[c] -= 1 # 완주한 참가자의 등장 횟수를 1감소
         
-    for pd in p_dict:
-        if p_dict[pd] == 1:
-            answer = pd
-    return answer
+    for pd in p_dict: # 딕셔너리를 반복
+        if p_dict[pd] == 1: # 등장 횟수가 1인 참가자를 찾음
+            answer = pd # 해당 참가자를 정답으로 설정
+    return answer # 찾은 정답을 반환 
