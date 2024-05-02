613c47c588404669e762ae9779b01bcec753cb48
verde <65857286+chorogrim@users.noreply.github.com>
2024-05-02 15:16:46 +0900

    Update 스택(큐)_주식가격.py

--- programmers/스택(큐)_주식가격.py
+++ programmers/스택(큐)_주식가격.py
@@ -1,7 +1,7 @@
 # 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때,
 # 가격이 떨어지지 않은 기간은 몇초인지를 return 하도록 solution 함수를 완성하세요.
 
-def solution(prices): # solution 함수를 정의하고, 하나의 매개변수 prices를 받음
+def solution(prices):
     answer = [] # 결과를 저장할 빈 리스트 answer를 초기화
     for i in range(len(prices)): # 입력 리스트 prices의 길이만큼 반복
         count = 0 # 현재 가격에서 유지된 기간을 세는 변수 count를 초기화
