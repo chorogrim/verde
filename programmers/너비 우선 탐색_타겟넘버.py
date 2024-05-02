7c11e18cbfbf9d0241ea8a6edcaf9f40122b287d
verde <65857286+chorogrim@users.noreply.github.com>
2024-05-02 14:32:44 +0900

    Update 너비 우선 탐색_타겟넘버.py

--- programmers/너비 우선 탐색_타겟넘버.py
+++ programmers/너비 우선 탐색_타겟넘버.py
@@ -1,17 +1,17 @@
 # 사용할 수 잇는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때
 # 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
 
-def solution(numbers, target):
-    answer = 0
-    ansList = [0]
+def solution(numbers, target): # numbers, target 두 개의 매개변수를 받음
+    answer = 0 # 경우의 수를 저장하기 위한 것으로 초기값 0으로 설정
+    ansList = [0] # 계산된 가능한 결과값을 저장하기 위한 리스트
     
-    for num in numbers:
-        tmpList = []
-        for a in ansList:
-            tmpList.append(a+num)
-            tmpList.append(a-num)
-        ansList = tmpList
-    for a in ansList:
-        if a == target:
-            answer += 1
-    return answer
+    for num in numbers: # 주어진 숫자 리스트인 numbers를 순회하면서 각 숫자를 하나씩 꺼내어 처리
+        tmpList = [] # 임시로 결과값을 저장하기 위한 리스트
+        for a in ansList: # ansList에 저장된 각각의 결과값에 대해 현재 숫자를 더하거나 빼서 나올 수 있는 모든 경우의 수를 tmpList에 저장
+            tmpList.append(a+num) # 현재 숫자 num을 이전 결과값 a에 더한 값을 tmpList에 추가
+            tmpList.append(a-num) # 현재 숫자 num을 이전 결과값 a에서 뺀 값을 tmpList에 추가
+        ansList = tmpList # 모든 계산이 끝나면 tmpList에 저장된 새로운 결과값들로 ansList를 업데이트
+    for a in ansList: # ansList에 저장된 각각의 값들을 순회하며 처리
+        if a == target: # 현재 값 a가 목표 수 target과 같은지 비교
+            answer += 1 # 만약 현재 값 a가 목표 수 target과 같다면, answer를 1증가
+    return answer # answer를 반환하여 함수의 결과로 출력
