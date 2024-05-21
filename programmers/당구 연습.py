798635d3c677fe3a9d69cef847f88a1013fec9ba
verde <65857286+chorogrim@users.noreply.github.com>
2024-05-21 15:11:30 +0900

    Update 당구 연습.py

--- programmers/당구 연습.py
+++ programmers/당구 연습.py
@@ -6,17 +6,17 @@ def solve(x, y, startX, startY, ballX, ballY):
         d2 = (ballX - startX)**2 + (ballY - 2*y+startY)**2 # 공과 위쪽 벽 사이의 거리를 계산
         dists.append(d2) # 계산된 거리를 리스트에 추가
     # 아래쪽 벽
-    # 단, x좌표가 같고 목표의 y가 더 작으면 안된다.
+    # 단, x좌표가 같고 목표의 y가 더 작으면 안됨
     if not (ballX == startX and ballY < startY):
         d2 = (ballX - startX)**2 + (ballY + startY)**2
         dists.append(d2)
     # 왼쪽 벽에 맞는 경우
-    # 단, y좌표가 같고 목표의 x가 더 작으면 안된다.
+    # 단, y좌표가 같고 목표의 x가 더 작으면 안됨
     if not (ballY == startY and ballX < startX):
         d2 = (ballX + startX)**2 + (ballY - startY)**2
         dists.append(d2)
     # 오른쪽 벽
-    # 단, y좌표가 같고 목표의 x가 더 크면 안된다.
+    # 단, y좌표가 같고 목표의 x가 더 크면 안됨
     if not (ballY == startY and ballX > startX):
         d2 = (ballX - 2*x+startX)**2 + (ballY - startY)**2
         dists.append(d2)
