e72a79c01e4f991084ef6092727cb8dd0eb7e058
verde <65857286+chorogrim@users.noreply.github.com>
2024-06-03 10:40:21 +0900

    Add files via upload

--- programmers/택배 배달과 수거하기.py
+++ programmers/택배 배달과 수거하기.py
@@ -1,11 +1,25 @@
 def solution(cap, n, deliveries, pickups):
-    d, p, answer = 0, 0, 0
-    for i in range(n-1, -1, -1): # 마지막 집부터 확인
-        d += deliveries[i] # 배달해야 할 양 확인
-        p += pickups[i] # 수거해야 할 양 확인
+    '''
+    cap: 한 번에 배달하거나 수거할 수 있는 최대 용량
+    n: 배달 및 수거해야 할 집의 수
+    deliveries: 각 집마다 배달해야 할 물건의 수를 담은 리스트
+    pickups: 각 집마다 수거해야 할 물건의 수를 담은 리스트
+    '''
+
+    deliveries = deliveries[::-1] # deliveries 리스트를 역순으로 뒤집기 (끝에서 시작)
+    pickups = pickups[::-1]
+    answer = 0
+    
+    d = 0 # 배달할 물건을 추적하기 위한 변수를 0으로 초기화
+    p = 0 # 수거해야 할 물건을 추적하기 위한 변수를 0으로 초기화
+    
+    for i in range(n):
+        d += deliveries[i] # 현재 집의 배달할 물건
+        p += pickups[i] # 수거할 물건
         
-        while d > 0 or p > 0: # 만약 둘 중 하나가 양수라면? 무조건 물류 창고를 돌려야한다
-            d -= cap # 물류 창고를 돌려야하니까 그만큼 배달 해준거니 빼주고
-            p -= cap # 수거도 마찬가지로 빼준다
-            answer += (i + 1)* 2 # 거리는 i+1에 왕복으로 갔다오니 *2
+        while d > 0 or p > 0:
+            d -= cap # 배달할 물건에서 한 번에 운반할 수 있는 최대 용량을 뺌
+            p -= cap # 수거할 물건에서 한 번에 운반할 수 있는 최대 용량을 뺌
+            answer += (n-i)*2 # (n-1)는 현재 집에서 끝까지의 거리, *2는 왕복 거리
+
     return answer
