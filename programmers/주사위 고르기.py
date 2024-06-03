0c1766d1527590d9b4ab0ed12012192a0663f64d
verde <65857286+chorogrim@users.noreply.github.com>
2024-06-03 14:36:15 +0900

    Update 주사위 고르기.py

--- programmers/주사위 고르기.py
+++ programmers/주사위 고르기.py
@@ -1,36 +1,29 @@
-def solution(h1, m1, s1, h2, m2, s2):
-    answer = 0
+def solution(h1, m1, s1, h2, m2, s2): # 시작 시간과 종료 시간을 받는 함수
+    answer = 0 # 만나는 횟수 저장할 변수 초기화
 
     # 시작시간과 끝시간을 초단위로 변환
-    startTime = h1 * 3600 + m1 * 60 + s1
-    endTime = h2 * 3600 + m2 * 60 + s2  
+    startTime = h1 * 3600 + m1 * 60 + s1 # 시작 시간을 초 단위로 계산
+    endTime = h2 * 3600 + m2 * 60 + s2 # 종료 시간을 초 단위로 계산
 
-    # next기준으로 계산할거니 포함되지 않는 시작시간 00시, 12시 미리 카운팅
-    if startTime == 0 * 3600 or startTime == 12 * 3600:
+    if startTime == 0 * 3600 or startTime == 12 * 3600: # 시작 시간이 0시(12시)인 경우 카운팅에 포함
         answer += 1
 
-    while startTime < endTime:
-        # 시침 1시간 = 30도 -> 1초에 30/3600도 즉, 1/120도 이동
-        # 분침 1분 = 6도 -> 1초에 6/60도 즉, 1/10도 이동
-        # 초침 1초 = 6도 -> 1초에 6도 이동 
-        hCurAngle = startTime / 120 % 360
-        mCurAngle = startTime / 10 % 360
-        sCurAngle = startTime * 6 % 360
+    while startTime < endTime: # 시작 시간부터 종료 시간까지 반복
+        hCurAngle = startTime / 120 % 360 # 시침의 현재 각도 계산
+        mCurAngle = startTime / 10 % 360 # 분침의 현재 각도 계산
+        sCurAngle = startTime * 6 % 360 # 초침의 현재 각도 계산
 
-        # 다음 위치가 360도가 아닌 0도로 계산되어 카운팅에 포함되지 않는 경우 방지
-        # 이동했을 때 지나쳤거나 같아졌는지를 비교하는 것이므로 현재위치는 해줄 필요없음
-        hNextAngle = 360 if (startTime + 1) / 120 % 360 == 0 else (startTime + 1) / 120 % 360
-        mNextAngle = 360 if (startTime + 1) / 10 % 360 == 0 else (startTime + 1) / 10 % 360
-        sNextAngle = 360 if (startTime + 1) * 6 % 360 == 0 else (startTime + 1) * 6 % 360
+        hNextAngle = 360 if (startTime + 1) / 120 % 360 == 0 else (startTime + 1) / 120 % 360 # 다음 초의 시침의 각도 계산
+        mNextAngle = 360 if (startTime + 1) / 10 % 360 == 0 else (startTime + 1) / 10 % 360 # 다음 초의 분침의 각도 계산
+        sNextAngle = 360 if (startTime + 1) * 6 % 360 == 0 else (startTime + 1) * 6 % 360 # 다음 초의 초침의 각도 계산
 
-        if sCurAngle < hCurAngle and sNextAngle >= hNextAngle:
+        if sCurAngle < hCurAngle and sNextAngle >= hNextAngle: # 초침이 시침과 만나는 경우 카운팅
             answer += 1
-        if sCurAngle < mCurAngle and sNextAngle >= mNextAngle:
+        if sCurAngle < mCurAngle and sNextAngle >= mNextAngle: # 초침이 분침과 만나는 경우 카운팅
             answer += 1
-        # 시침/분침과 동시에 겹쳤을 때 중복카운팅 제외 
-        if sNextAngle == hNextAngle and hNextAngle == mNextAngle:
+        if sNextAngle == hNextAngle and hNextAngle == mNextAngle: # 시침,분침이 동시에 겹쳤을 때 중복 카운팅 제외 
             answer -= 1
 
-        startTime += 1
+        startTime += 1 # 시작 시간을 1초씩 증가시키며 반복 진행
     
-    return answer
+    return answer # 최종적으로 만나는 횟수 반환
