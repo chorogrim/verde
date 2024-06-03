544d948993936850ef9d183897d1114746748cd7
verde <65857286+chorogrim@users.noreply.github.com>
2024-06-03 11:32:27 +0900

    Update 행렬 테두리 회전하기.py

--- programmers/행렬 테두리 회전하기.py
+++ programmers/행렬 테두리 회전하기.py
@@ -1,4 +1,9 @@
 def solution(rows, columns, queries):
+    '''
+    rows: 행의 수
+    columns: 열의 수
+    queries: 회전할 영역을 정의하는 쿼리 리스트
+    '''
     answer = [] # 최종적으로 반환할 결과를 저장할 빈 리스트
     arr = [] # 행렬을 저장할 빈 리스트
     cnt = 1 # 시작 숫자를 나타내는 변수 cnt를 1로 초기화
@@ -12,49 +17,54 @@ def solution(rows, columns, queries):
         x_len, y_len = x2 - x1, y2 - y1 # x와 y의 길이를 구함
         num_list = [] # 현재 쿼리에서 회전된 숫자들을 저장할 빈 리스트 
         s_x, s_y = x1 - 1, y1 - 1 # 행렬 상에서의 시작점 좌표를 설정
+        tmp = arr[s_x][s_y] # 시작점의 값을 tmp에 저장
 
-        tmp = arr[s_x][s_y]
+        # 우측 이동
         for i in range(1, y_len + 1):
             tmp = arr[s_x][s_y + i]
-            num_list.append(tmp)
-        s_y += y_len
+            num_list.append(tmp) 
+        s_y += y_len # y좌표를 오른쪽 끝으로 업데이트
 
+        # 아래쪽 이동
         for i in range(1, x_len + 1):
             tmp = arr[s_x + i][s_y]
             num_list.append(tmp)
-        s_x += x_len
+        s_x += x_len # x좌표를 아래쪽 끝으로 업데이트
 
+        # 좌측 이동
         for i in range(1, y_len + 1):
             tmp = arr[s_x][s_y - i]
             num_list.append(tmp)
-        s_y -= y_len
+        s_y -= y_len # y 좌표를 왼쪽 끝으로 업데이트
 
+        # 위쪽 이동
         for i in range(1, x_len + 1):
             tmp = arr[s_x - i][s_y]
             num_list.append(tmp)
-        s_x -= x_len
+        s_x -= x_len # x좌표를 위쪽 끝으로 업데이트
 
-        answer.append(min(num_list))
+        answer.append(min(num_list)) # num_list에서 최솟값을 answer 리스트에 추가
 
         cnt = -1
+        # 우측 이동
         for i in range(1, y_len + 1):
             arr[s_x][s_y + i] = num_list[cnt]
             cnt += 1
-        s_y += y_len
+        s_y += y_len # y 좌표를 오른쪽 끝으로 업데이트
 
+        # 아래쪽 이동
         for i in range(1, x_len + 1):
             arr[s_x + i][s_y] = num_list[cnt]
             cnt += 1
         s_x += x_len
 
+        # 좌측 이동
         for i in range(1, y_len + 1):
             arr[s_x][s_y - i] = num_list[cnt]
             cnt += 1
         s_y -= y_len
 
+        # 위쪽 이동
         for i in range(1, x_len + 1):
             arr[s_x - i][s_y] = num_list[cnt]
             cnt += 1
-        s_x -= x_len
-
-    return answer
